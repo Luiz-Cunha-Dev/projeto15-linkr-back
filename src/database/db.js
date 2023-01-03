f9e494cc068bfb886fcde35fa5720a6341b85524
Luiz-Cunha-Dev <luizcunha.dev99@gmail.com>
2023-01-03 19:19:07 -0300

    initial structure

---
+++ src/database/db.js
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+dotenv.config();
+import pg from 'pg';
+const { Pool } = pg;
+
+export let connection;
+
+try {
+   connection = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+  });
+} catch (err) {
+    console.log("Erro ao conectar no banco de dados: ", err);
+}
+
