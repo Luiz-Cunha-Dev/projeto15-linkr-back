8149bc7ff5be86fee0df34458f2a02419e8b10bc
Luiz-Cunha-Dev <luizcunha.dev99@gmail.com>
2023-01-05 11:36:20 -0300

    feat: search user route and search user controller

--- src/controllers/users.Controller.js
+++ src/controllers/users.Controller.js
@@ -1,7 +1,8 @@
-import { getSessionByToken, deleteSession } from "../repository/auth.repository.js";
+import { getSessionByToken, getUsersByName } from "../repository/users.repository.js";
 
-export async function logout(req, res){
+export async function getUsers(req, res){
     const {authorization} = req.headers;
+    const {name} = req.body;
 
     if(!authorization){
         res.sendStatus(401);
@@ -11,17 +12,17 @@ export async function logout(req, res){
     const token = authorization.replace("Bearer ", "");
 
     try{
-        const session = await getSessionByToken(token);
+        const session = await getSessionByToken(token)
 
         if(session.rows.length === 0){
             res.sendStatus(401)
             return
         }
 
-        await deleteSession(session.rows[0].userId)
-    
-        res.sendStatus(200);
-        
+        const users = await getUsersByName(name)
+
+        res.send(users.rows).status(200)
+
     }catch(err){
         console.log(err);
         res.sendStatus(500)
