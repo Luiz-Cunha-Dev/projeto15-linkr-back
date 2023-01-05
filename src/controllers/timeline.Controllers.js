a8e01585333aba42280f2aaaae7c2dc3ed6ca80a
mcrestani <marianascrestani@gmail.com>
2023-01-05 15:22:11 -0300

    feat: update and delete posts on timeline

--- src/controllers/timeline.Controllers.js
+++ src/controllers/timeline.Controllers.js
@@ -1,3 +1,11 @@
+import { getSessionByToken } from "../repository/auth.repository.js";
+import {
+  deleteOnePost,
+  insertPost,
+  insertUpdatedPost,
+  selectUserId,
+} from "../repository/timeline.repository.js";
+
 export async function createPost(req, res) {
   const { authorization } = req.headers;
 
@@ -17,11 +25,83 @@ export async function createPost(req, res) {
     }
     const userId = session.useId;
     const link = post.link;
-    const comment = post.comment;
+    const comments = post.comments;
 
-    await insertPost(userId, link, comment);
+    await insertPost(userId, link, comments);
 
     res.sensStatus(201);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+}
+
+export async function updatePost(req, res) {
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    res.sendStatus(401);
+    return;
+  }
+  const token = authorization.replace("Bearer ", "");
+  const post = req.res; 
+
+  try {
+    const session = await getSessionByToken(token);
+
+    if (session.rows.length === 0) {
+      res.sendStatus(401);
+      return;
+    }
+
+    const id = post.id;
+    const comments = post.comments;
+
+    const postUserId = await selectUserId(id);
+
+    if (postUserId !== session.useId) {
+      res.sendStatus(401);
+      return;
+    }
+
+    await insertUpdatedPost(comments, id);
+
+    res.sensStatus(200);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+}
+
+
+export async function deletePost(req, res) {
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    res.sendStatus(401);
+    return;
+  }
+  const token = authorization.replace("Bearer ", "");
+  const post = req.res; 
+
+  try {
+    const session = await getSessionByToken(token);
+
+    if (session.rows.length === 0) {
+      res.sendStatus(401);
+      return;
+    }
+
+    const id = post.id;
+
+    const postUserId = await selectUserId(id);
+
+    if (postUserId !== session.useId) {
+      res.sendStatus(401);
+      return;
+    }
+
+    await deleteOnePost(id);
+
+    res.sensStatus(200);
   } catch (err) {
     res.status(500).send(err.message);
   }
--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
-import { createPost } from "../controllers/timeline.Controllers.js";
+import {
+  createPost,
+  deletePost,
+  updatePost,
+} from "../controllers/timeline.Controllers.js";
 import { schemaValidation } from "../middlewares/schemaValidation.middleware.js";
 import { postsSchema } from "../models/schemas.js";
 
@@ -7,7 +11,7 @@ const postsRouter = Router();
 
 postsRouter.get("/timeline");
 postsRouter.post("/timeline", schemaValidation(postsSchema), createPost);
-postsRouter.patch("/timeline");
-postsRouter.delete("/timeline");
+postsRouter.patch("/timeline"), updatePost;
+postsRouter.delete("/timeline"), deletePost;
 
 export default postsRouter;
