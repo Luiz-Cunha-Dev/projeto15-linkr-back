ad5e60a831745aa0e44b4d64ee841cf1f9843a84
Luiz-Cunha-Dev <luizcunha.dev99@gmail.com>
2023-01-06 15:43:32 -0300

    feat: get user posts

--- src/controllers/users.Controller.js
+++ src/controllers/users.Controller.js
@@ -1,4 +1,4 @@
-import { getSessionByToken, getUsersByName } from "../repository/users.repository.js";
+import { getPostById, getSessionByToken, getUserbyId, getUsersByName } from "../repository/users.repository.js";
 
 export async function getUsers(req, res){
     const {name} = req.body;
@@ -14,6 +14,25 @@ export async function getUsers(req, res){
         const users = await getUsersByName(name)
 
         res.send(users.rows).status(200)
+
+    }catch(err){
+        console.log(err);
+        res.sendStatus(500)
+    }
+}
+
+
+export async function getPostByUser(req, res){
+    const {id} = req.params;
+
+    try{
+        const user = await getUserbyId(id)
+
+        const userPosts = await getPostById(id)
+
+        user.rows[0].posts = userPosts.rows
+
+        res.send(user.rows[0]).status(200)
 
     }catch(err){
         console.log(err);
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { getUsers } from "../controllers/users.Controller.js";
+import { getPostByUser, getUsers } from "../controllers/users.Controller.js";
 
 const usersRouter = Router();
 
 usersRouter.get("/users", getUsers);
+
+usersRouter.get("/users/:id", getPostByUser);
 
 export default usersRouter;
