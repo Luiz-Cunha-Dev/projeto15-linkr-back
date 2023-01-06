17b8ca63020b5a6b39673dbdacac8b16669b4a70
Luiz-Cunha-Dev <luizcunha.dev99@gmail.com>
2023-01-06 17:54:11 -0300

    old version

--- src/controllers/users.Controller.js
+++ src/controllers/users.Controller.js
@@ -1,17 +1,10 @@
-import { getSessionByToken, getUsersByName } from "../repository/users.repository.js";
+import { getPostById, getUserbyId, getUsersByName } from "../repository/users.repository.js";
 
 export async function getUsers(req, res){
-    const {name} = req.body;
+    const {username} = req.body;
 
     try{
-        const session = await getSessionByToken(token)
-
-        if(session.rows.length === 0){
-            res.sendStatus(401)
-            return
-        }
-
-        const users = await getUsersByName(name)
+        const users = await getUsersByName(username)
 
         res.send(users.rows).status(200)
 
